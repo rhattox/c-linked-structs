7889a2754c928e6e7230660d3f69d80ab4887ab2
Bruno Coviello <bcovies@gmail.com>
2024-03-02 13:11:16 -0300

    Add new pointer

--- pointer.c
+++ pointer.c
@@ -7,15 +7,15 @@ int main(int argc, char *argv[]) {
   int8_t var1 = 10;
   int8_t *var_pointer;
  
-  printf("var1: %d\n",var1);
+  printf("var1 initial value: %d\n",var1);
 
   var_pointer = &var1;
   
-  printf("var_pointer: %p\n",var_pointer);
-
   *var_pointer = 100;
 
-  printf("var1: %d\n", var1);
+  printf("Pointer recieves new value\n");
+
+  printf("var1 final value: %d\n", var1);
  
   return EXIT_SUCCESS; 
 }
