c4bd01782fb67355bca657e88053c71681ec4ef6
Bruno Coviello <bcovies@gmail.com>
2024-03-11 22:16:23 -0300

    add push and list, pop is not working correctly

--- basic-lifo/lifo.c
+++ basic-lifo/lifo.c
@@ -2,17 +2,26 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int* pushNewItem(int* arr, int* size){
-  printf("\nThis is the size %d", *size);
+void *listArray(int *arr, int *size){
+  printf("\n\n");
+  for(int i=0;i<*size;i++){
+    printf("\n[%d]: %d",i,arr[i]);
+  }
+  printf("\n\n");
+}
+
+void *popLastItem(int *size){
+  
+  *size = *size - 1;
+
+
+}
+
+void *pushNewItem(int *arr, int *size){
   *size = *size + 1;
   printf("\nAdding a new item: ");
   scanf("%d",&arr[*size-1]);
 
-  for(int i=0;i<*size;i++){
-    printf("\n\n");
-    printf("[%d]: %d",i,arr[i]);
-  }
-  return arr;
 }
 
 int main(int argc, char *argv[])
@@ -35,33 +44,40 @@ int main(int argc, char *argv[])
     printf("\n[%d]: %d",i,stack[i]);
   }
   
-  int* stackPtr = stack;
-  int* sizePtr = &size;
+  int *stackPtr = stack;
+  int *sizePtr = &size;
 
   bool whileControl = true;
   while (whileControl) {
     printf("\nSelect what you want to do now: \n");
-
+    printf("\n1. Push a new item (last) : \n");
+    printf("\n2. Pop an item (last): \n");
+    printf("\n3. List the array: \n");
+    printf("\n0. Exit the program: \n");
+    
     int switchChoice;
 
     scanf("%d", &switchChoice);
 
     switch (switchChoice) {
       case 1: 
-        printf("\nPush a new Element");
-        pushNewItem(stackPtr,  sizePtr);
+        printf("\nPush a new Element\n");
+        pushNewItem(stackPtr, sizePtr);
       break;
       case 2:
-        printf("Pop an Element");
+        printf("\nPop an Element\n");
+        popLastItem(stackPtr);
       break;
       case 3:
-        printf("Check a new pool");
+        printf("\nListing the array\n");
+        listArray(stackPtr, sizePtr);
       break;
       case 0:
+        printf("\n\nExit the program\n\nbye!\n\n");
         whileControl = false;
       break;
       default:
-        printf("Not a valid choice!");
+        printf("\nNot a valid choice!\n");
 
     }
   }
