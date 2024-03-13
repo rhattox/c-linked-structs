6b64ea6894b688a00d044c3281e56713603f4fb0
Bruno Coviello <bcovies@gmail.com>
2024-03-12 22:31:54 -0300

    Add input and outputs

--- basic-lifo/lifo.c
+++ basic-lifo/lifo.c
@@ -4,16 +4,26 @@
 
 void *listArray(int *arr, int *size){
   printf("\n\n");
-  for(int i=0;i<*size;i++){
-    printf("\n[%d]: %d",i,arr[i]);
+  if (*size <= 0){
+    printf("Please, initialize the array!");
+  }
+  else {
+    for(int i=0;i<*size;i++){
+      printf("\n[%d]: %d",i,arr[i]);
+    }
   }
   printf("\n\n");
 }
 
 void *popLastItem(int *size){
-  printf("%d", *size);
-  *size = *size - 1;
-  printf("%d", *size);
+  if(*size > 0){
+    printf("\nCurrent Size: %d", *size);
+    *size = *size - 1;
+  }
+  if (*size == 0){
+    printf("\nCurrent Size is: %d, please, push new element!", *size);
+
+  }
 }
 
 void *pushNewItem(int *arr, int *size){
@@ -25,50 +35,59 @@ void *pushNewItem(int *arr, int *size){
 
 int main(int argc, char *argv[])
 {
+  printf("<===================================>");
   printf("\nStarting...\n");
 
   int size;
 
+  printf("<===================================>");
   printf("\nWhat is the size of the array?\n");
+  printf("$ ");
   scanf("%d",&size);
   
   int stack[size];
 
   for(int i=0;i<size;i++){
-    printf("\n\tWhat is the [%d]: ",i);
+    printf("\n\t Enter the element [%d]: ",i);
     scanf("%d",&stack[i]);
   }
 
+  printf("<===================================>");
+  printf("\n");
+  printf("Here is the current array: ");
   for(int i=0;i<size;i++){
     printf("\n[%d]: %d",i,stack[i]);
   }
+  printf("\n");
   
   int *stackPtr = stack;
   int *sizePtr = &size;
 
   bool whileControl = true;
   while (whileControl) {
+    printf("<===================================>");
     printf("\nSelect what you want to do now: \n");
     printf("\n1. Push a new item (last) : \n");
     printf("\n2. Pop an item (last): \n");
     printf("\n3. List the array: \n");
     printf("\n0. Exit the program: \n");
-    
+    printf("<===================================>");
+
     int switchChoice;
 
+    printf("\n");
+    printf("$ ");
     scanf("%d", &switchChoice);
-
+    printf("<===================================>");
+    
     switch (switchChoice) {
       case 1: 
-        printf("\nPush a new Element\n");
         pushNewItem(stackPtr, sizePtr);
       break;
       case 2:
-        printf("\nPop an Element\n");
         popLastItem(sizePtr);
       break;
       case 3:
-        printf("\nListing the array\n");
         listArray(stackPtr, sizePtr);
       break;
       case 0:
